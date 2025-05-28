2fe4e47478e96e14b9b469225d5e25f6902a29fd
opel-astra-g <kis.csaba.d@gmail.com>
2025-05-28 12:21:56 +0200

    alap kész?

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-szekek = [1, 2, 3, 4, 5, 6, 7, 8]
-
 melyik_szek = int(input("Melyik széket szeretnéd lefoglalni? (1-8): "))
 while not (melyik_szek == 1 or melyik_szek == 2 or melyik_szek == 3 or melyik_szek == 4 or melyik_szek == 5 or melyik_szek == 6 or melyik_szek == 7 or melyik_szek == 8):
     melyik_szek = int(input("Melyik széket szeretnéd lefoglalni? (1-8): "))
@@ -9,9 +7,5 @@ foglaltak  = ["🟢", "🟢", "🟢", "🟢","🟢", "🟢", "🟢", "🟢",]
 foglaltak.pop(melyik_szek)
 foglaltak.insert(melyik_szek-1, "🔴")
 
-
 print("🔴 ha szék foglalt, 🟢 ha nem foglalt")
-print(szekek)
 print(foglaltak)
-
-#extra commit?
