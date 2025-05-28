01ba72f077d631f034731f685669a86c10a8a9f9
opel-astra-g <kis.csaba.d@gmail.com>
2025-05-28 13:09:24 +0200

    extra dolog, nem tökéletes

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-melyik_szek = int(input("Melyik széket szeretnéd lefoglalni? (1-8): "))
-while not (1 <= melyik_szek <= 8):
-    melyik_szek = int(input("Melyik széket szeretnéd lefoglalni? (1-8): "))
-
 foglaltak = []
 i = 0
 
@@ -9,8 +5,21 @@ while i < 8:
     foglaltak.append("🟢")
     i += 1
 
-foglaltak.pop(melyik_szek)
-foglaltak.insert(melyik_szek-1, "🔴")
+def foglalas():
+    melyik_szek = int(input("Melyik széket szeretnéd lefoglalni? (1-8): "))
+    while not (1 <= melyik_szek <= 8):
+        melyik_szek = int(input("Melyik széket szeretnéd lefoglalni? (1-8): "))
+    foglaltak.pop(melyik_szek)
+    foglaltak.insert(melyik_szek-1, "🔴")
+    print("🔴 ha szék foglalt, 🟢 ha nem foglalt")
+    print(foglaltak)
 
-print("🔴 ha szék foglalt, 🟢 ha nem foglalt")
-print(foglaltak)
+    megegy = input("Szeretnél mégegy széket foglalni? (igen-nem)")
+    while not (megegy == "igen" or megegy == "nem"):
+        megegy = input("Szeretnél mégegy széket foglalni? (igen-nem)")
+    if megegy == "igen":
+        foglalas()
+    elif megegy == "nem":
+        breakpoint
+
+foglalas()
