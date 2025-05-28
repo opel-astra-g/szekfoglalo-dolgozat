927c334e5af676d99c2058589d21cf85eecc5b3b
opel-astra-g <kis.csaba.d@gmail.com>
2025-05-28 12:57:27 +0200

    "Első commit", csak megjelenítés

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-melyik_szek = int(input("Melyik széket szeretnéd lefoglalni? (1-8): "))
-while not (1 <= melyik_szek <= 8):
-    melyik_szek = int(input("Melyik széket szeretnéd lefoglalni? (1-8): "))
-
 foglaltak = []
 i = 0
 
@@ -9,10 +5,6 @@ while i < 8:
     foglaltak.append("🟢")
     i += 1
 
-foglaltak.pop(melyik_szek)
-foglaltak.insert(melyik_szek-1, "🔴")
-
 print("🔴 ha szék foglalt, 🟢 ha nem foglalt")
 print(foglaltak)
 
-# szünet
