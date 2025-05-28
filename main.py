df37b0fbd845dc28179c3242a5db4e8d2eda01a5
opel-astra-g <kis.csaba.d@gmail.com>
2025-05-28 12:09:40 +0200

    extra commit

--- main.py
+++ main.py
@@ -4,14 +4,14 @@ melyik_szek = int(input("Melyik széket szeretnéd lefoglalni? (1-8): "))
 while not (melyik_szek == 1 or melyik_szek == 2 or melyik_szek == 3 or melyik_szek == 4 or melyik_szek == 5 or melyik_szek == 6 or melyik_szek == 7 or melyik_szek == 8):
     melyik_szek = int(input("Melyik széket szeretnéd lefoglalni? (1-8): "))
 
-foglaltak  = []
+foglaltak  = ["🟢", "🟢", "🟢", "🟢","🟢", "🟢", "🟢", "🟢",]
 
-i = 0
+foglaltak.pop(melyik_szek)
+foglaltak.insert(melyik_szek-1, "🔴")
 
-while 
-
-szekek.pop(melyik_szek-1)
 
 print("🔴 ha szék foglalt, 🟢 ha nem foglalt")
 print(szekek)
-print(f"{}")
+print(foglaltak)
+
+#extra commit?
