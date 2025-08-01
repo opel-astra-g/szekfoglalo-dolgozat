5fd851514ccbb89904f2b8282c2de10f1e7d07ea
opel-astra-g <kis.csaba.d@gmail.com>
2025-08-01 08:30:11 +0200

    egyszerűsítés, break kivétele

--- main.py
+++ main.py
@@ -1,24 +1,18 @@
-szekek = []
-szekek_szama = 8
+szekek_szama = 10
+szekek = ["🟢"]*szekek_szama
 
-for i in range(szekek_szama):
-    szekek.append("🟢")
+loop = 0
 
-print(szekek)
-
-while True:
-
+while loop == 0:
     melyik_szekek = input(f"Melyik széket szeretnéd lefoglalni? (1-{szekek_szama}) Írd be hogy 'kilép' a kilépéshez.\n>")
     if melyik_szekek == "kilép":
-        break
+        loop = 1
     try:
         int_szek = int(melyik_szekek)-1
 
         if not 0 <= int_szek < szekek_szama:
-            raise ValueError
-        szekek.pop(int_szek)
-        szekek.insert(int_szek, "🔴")
-    except (ValueError, TypeError):
+            raise Exception
+        szekek[int_szek] = "🔴"
+        print(szekek)
+    except (ValueError, TypeError, Exception):
         print(f"Helytelen érték! Csak számok 1-{szekek_szama}ig")
-        continue
-    print(szekek)
