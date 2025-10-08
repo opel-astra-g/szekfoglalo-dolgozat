eadef7d86b8d95f84155aaa1114a7aafa5588b62
opel-astra-g <ki.csaba.d@gmail.com>
2025-10-08 10:57:42 +0200

    szerkezeti, logikai valtozas

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 SZEKEK_SZAMA = 10
 szekek = ["🟢"]*SZEKEK_SZAMA
 
-loop = 0
+loop = True
 
-while loop == 0:
+while loop:
     melyik_szekek = input(f"Melyik széket szeretnéd lefoglalni? (1-{SZEKEK_SZAMA}) Írd be hogy 'kilép' a kilépéshez.\n>")
     if melyik_szekek == "kilép":
-        loop = 1
-    try:
-        int_szek = int(melyik_szekek)-1
+        loop = False
+    else:
+        try:
+            int_szek = int(melyik_szekek)-1
 
-        if not 0 <= int_szek < SZEKEK_SZAMA:
-            raise Exception
-        szekek[int_szek] = "🔴"
-        print(szekek)
-    except (ValueError, TypeError, Exception):
-        print(f"Helytelen érték! Csak számok 1-{SZEKEK_SZAMA}ig")
+            if not 0 <= int_szek < SZEKEK_SZAMA:
+                raise 
+            szekek[int_szek] = "🔴"
+            print(szekek)
+        except:
+            print(f"Helytelen érték! Csak számok 1-{SZEKEK_SZAMA}ig")
