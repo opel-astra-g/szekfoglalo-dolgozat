fbe7f6b065deff8b1afc3959ee65dd37d5ae48a3
opel-astra-g <ki.csaba.d@gmail.com>
2025-10-15 11:16:51 +0200

    extra funkciok

--- main.py
+++ main.py
@@ -4,16 +4,22 @@ szekek = ["🟢"]*SZEKEK_SZAMA
 loop = True
 
 while loop:
-    melyik_szekek = input(f"Melyik széket szeretnéd lefoglalni? (1-{SZEKEK_SZAMA}) Írd be hogy 'kilép' a kilépéshez.\n>")
+    melyik_szekek = input(f"Melyik széket szeretnéd lefoglalni? (1-{SZEKEK_SZAMA}) Írd be hogy 'kilép' a kilépéshez. Írd be hogy 'mutasd' a székek állapotának látásához.\n>")
     if melyik_szekek == "kilép":
         loop = False
+    elif melyik_szekek == "mutasd":
+        print(szekek)
+        continue
     else:
         try:
             int_szek = int(melyik_szekek)-1
 
             if not 0 <= int_szek < SZEKEK_SZAMA:
                 raise 
+            if "🔴" in szekek[int_szek]:
+                print("Ez a szék már le van foglalva!")
+                continue
             szekek[int_szek] = "🔴"
             print(szekek)
         except:
-            print(f"Helytelen érték! Csak számok 1-{SZEKEK_SZAMA}ig")
+            print(f"Helytelen érték! Csak számok 1-{SZEKEK_SZAMA}ig!")
